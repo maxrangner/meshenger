dc2b99b502e2541309ae3968b3eadf179fd55652
maxrangner <max@rangner.com>
2026-07-19 20:15:03 +0200

    feat: add Kconfig option to choose sender or receiver firmware

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,29 @@
 #include "freertos/FreeRTOS.h"
 #include "radio_service.h"
+#include "sdkconfig.h"
 #include "esp_log.h"
 
 static const char *TAG = "main";
+
+static void runSender(RadioService *radioService) {
+    int state = radioService->startSend();
+    if (state != RADIOLIB_ERR_NONE) {
+        ESP_LOGI(TAG, "Failed to start sending, code %d\n", state);
+    }
+    while(true) {
+
+    }
+}
+
+static void runReceiver(RadioService *radioService) {
+    int state = radioService->startReceive();
+    if (state != RADIOLIB_ERR_NONE) {
+        ESP_LOGI(TAG, "Failed to start receiving, code %d\n", state);
+    }
+    while(true) {
+
+    }
+}
 
 extern "C" void app_main(void)
 {
@@ -15,13 +36,9 @@ extern "C" void app_main(void)
         ESP_LOGI(TAG, "Radio failed to initialize, code %d\n", state);
     }
 
-    state = radioService.startReceive();
-    if (state != RADIOLIB_ERR_NONE) {
-        ESP_LOGI(TAG, "Failed to start receiving, code %d\n", state);
-    }
-
-    while (true) {
-        ESP_LOGI(TAG, "Waiting for packet...");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+    #if CONFIG_MESHERNGER_ROLE_SENDER
+        runSender(&radioService);
+    #else
+        runReceiver(&radioService);
+    #endif
 }
