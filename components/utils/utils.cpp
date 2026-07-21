e7d63295cb1dd2c2c5d7639c2bd88fe551efa7ed
maxrangner <max@rangner.com>
2026-07-22 01:58:58 +0200

    refactor(utils): add static casts to packet conversion

--- components/utils/utils.cpp
+++ components/utils/utils.cpp
@@ -9,16 +9,16 @@ void serialize_packet(const protocol::Packet& packet, uint8_t* buffer) {
     buffer[1] = packet.sender_id;
     // memcopy
     for (int i = 0; i < protocol::kPacketSize - 2; i++) {
-        buffer[i + 2] = packet.payload[i];
+        buffer[i + 2] = static_cast<uint8_t>(packet.payload[i]);
     }
 }
 
 void deserialize_packet(const uint8_t* buffer, protocol::Packet& packet) {
-    packet.version = buffer[0];
-    packet.sender_id = buffer[1];
+    packet.version = static_cast<uint8_t>(buffer[0]);
+    packet.sender_id = static_cast<uint8_t>(buffer[1]);
     // memcopy
     for (int i = 0; i < protocol::kPacketSize - 2; i++) {
-        packet.payload[i] = buffer[i + 2];
+        packet.payload[i] = static_cast<char>(buffer[i + 2]);
     }
     packet.payload[protocol::kPacketSize - 3] = '\0';
 }
